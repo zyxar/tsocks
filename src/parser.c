08fb59cd05fbb29a66097e5d7b041c68f7e4bc14
Marcus Zy <chou.marcus@gmail.com>
2013-01-24 17:08:12 +0800

    manual add cmdline --server --port --local support in parser.c and doc

--- src/parser.c
+++ src/parser.c
@@ -121,6 +121,27 @@ int read_config (char *filename, struct parsedfile *config) {
         handle_local(config, 0, "169.254.0.0/255.255.0.0");
         handle_local(config, 0, "192.168.0.0/255.255.0.0");
 
+#ifdef ALLOW_ENV_CONFIG
+    /* Add settings from env */
+    {
+        const char *env;
+
+        /* create appropriate lines */
+        if ((env = getenv ("TORSOCKS_SERVER")) != NULL) {
+            snprintf (line, MAXLINE, "server = %s", env);
+            handle_line (config, line, 0);
+        }
+        if ((env = getenv ("TORSOCKS_SERVER_PORT")) != NULL) {
+            snprintf (line, MAXLINE, "server_port = %s", env);
+            handle_line (config, line, 0);
+        }
+        if ((env = getenv ("TORSOCKS_LOCAL")) != NULL) {
+            snprintf (line, MAXLINE, "%s", env);
+            handle_local(config, 0, line);
+        }
+    }
+#endif
+
         /* Check default server */
         check_server(&(config->defaultserver));
         server = (config->paths);
@@ -370,11 +391,10 @@ static int handle_server(struct parsedfile *config, int lineno, char *value) {
     if (currentcontext->address == NULL)
         currentcontext->address = strdup(ip);
     else {
-        if (currentcontext == &(config->defaultserver))
-            show_msg(MSGERR, "Only one default SOCKS server "
-                   "may be specified at line %d in "
-                   "configuration file\n", lineno);
-        else
+        if (currentcontext == &(config->defaultserver)) {
+            free (currentcontext->address);
+            currentcontext->address = strdup(ip);
+        } else
             show_msg(MSGERR, "Only one SOCKS server may be specified "
                    "per path on line %d in configuration "
                    "file. (Path begins on line %d)\n",
@@ -387,11 +407,7 @@ static int handle_server(struct parsedfile *config, int lineno, char *value) {
 static int handle_port(struct parsedfile *config, int lineno, char *value) {
 
     if (currentcontext->port != 0) {
-        if (currentcontext == &(config->defaultserver))
-            show_msg(MSGERR, "Server port may only be specified "
-                   "once for default server, at line %d "
-                   "in configuration file\n", lineno);
-        else
+        if (currentcontext != &(config->defaultserver))
             show_msg(MSGERR, "Server port may only be specified "
                    "once per path on line %d in configuration "
                    "file. (Path begins on line %d)\n",
